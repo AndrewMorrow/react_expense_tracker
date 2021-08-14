6fb43628655dd81bec8e7de7fb37ab6fc4c9331a
AndrewMorrow <beardedmongo@gmail.com>
2021-08-14 11:45:39 -0500

    add and delete functionality added, app complete

--- expense-tracker/src/components/TransactionList.js
+++ expense-tracker/src/components/TransactionList.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../store/GlobalState";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
-      <ul className="list"></ul>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
     </>
   );
 };
